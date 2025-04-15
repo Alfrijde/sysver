2d2cf70baacecef30abb3b5e2171a2c81174756a
Viktoria Arvidsson <viav23@student.bth.se>
2025-04-15 10:55:41 +0200

    New tests added

---
+++ backend/test/test_usercontroller.py
@@ -0,0 +1,63 @@
+import pytest
+import unittest.mock as mock
+
+from src.controllers.usercontroller import UserController
+
+def test_get_user_by_email_valid_unique():
+    # Test if the function returns one user with an unique email.
+    mockedDAO = mock.MagicMock()
+
+    mockedDAO.find.return_value = [{"user":"A user"}]
+
+    sut = UserController(dao=mockedDAO)
+
+    result = sut.get_user_by_email(email="hi@hello.com")
+
+    assert result == {"user":"A user"}
+
+def test_get_user_by_email_valid_not_unique():
+    # Test if the function returns one user with a not unique email. Should return the first user in the list.
+    mockedDAO = mock.MagicMock()
+
+    mockedDAO.find.return_value = [{"user":"Another user"}, {"user":"A user"}]
+
+    sut = UserController(dao=mockedDAO)
+
+    result = sut.get_user_by_email(email="hi@hello.com")
+
+    assert result == {"user":"Another user"}
+
+def test_get_user_by_email_valid_not_in_db():
+    # Test if None is returned when an email is not in the database
+    mockedDAO = mock.MagicMock()
+
+    mockedDAO.find.return_value = []
+
+    sut = UserController(dao=mockedDAO)
+
+    result = sut.get_user_by_email(email="hi@hello.com")
+
+    assert result == None
+
+def test_get_user_by_email_invalid_email():
+    # Assert that an invalid email raises a ValueError
+    with pytest.raises(ValueError):
+        mockedDAO = mock.MagicMock()
+
+        mockedDAO.find.return_value = [{"user":"A user"}]
+
+        sut = UserController(dao=mockedDAO)
+
+        result = sut.get_user_by_email(email="hello.com")
+
+def test_get_user_by_email_db_fail():
+    # Test an Exception is raised if the database fails
+    with pytest.raises(Exception):
+        mockedDAO = mock.MagicMock()
+
+        mockedDAO.find.return_value = None
+
+        sut = UserController(dao=mockedDAO)
+
+        result = sut.get_user_by_email(email="hi@hello.com")
+
