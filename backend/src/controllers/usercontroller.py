70d98650950497ad13d56a5c7f16e897368f8139
Viktoria Arvidsson <viav23@student.bth.se>
2025-05-13 14:31:10 +0200

    Updated usercontroller

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -33,7 +33,7 @@ class UserController(Controller):
             if len(users) == 1:
                 return users[0]
             else:
-                print(f'Error: more than one user found with mail {email}')
+                print(f'Error: more than one user found with mail {email}', flush=True)
                 return users[0]
         except Exception as e:
             raise
